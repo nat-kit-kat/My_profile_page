df5028ebc9c6c0b8e456a7dbff529640d0c9b7a5
Natalia Baranova <56912400+nat-kit-kat@users.noreply.github.com>
2024-02-11 20:17:56 +0300

    Adding files

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, redirect, url_for
+app = Flask(__name__)
+
+@app.route('/profile')
+def index():
+    return render_template("index.html")
+
+@app.route('/')
+def redirect_to_profile():
+    return redirect(url_for('index'))
+
+app.run(host="localhost", port=5000, debug=True)
